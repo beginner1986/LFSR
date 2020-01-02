d29d96412819f32de860c481c83f20c33027e672
Adam Emieljaniuk <beginner1986@gmail.com>
2020-01-02 22:01:20 +0100

    New UI project done.

--- LFSR/MainWindow.xaml.cs
+++ LFSR/MainWindow.xaml.cs
@@ -24,32 +24,5 @@ namespace LFSR
         {
             InitializeComponent();
         }
-
-        private void Button1Click(object sender, RoutedEventArgs e)
-        {
-            function1.Text = GenerateFunction(int.Parse(lenth1.Text));
-        }
-
-        private void Button2Click(object sender, RoutedEventArgs e)
-        {
-            function2.Text = GenerateFunction(int.Parse(lenth2.Text));
-        }
-        
-        private void Button3Click(object sender, RoutedEventArgs e)
-        {
-            function3.Text = GenerateFunction(int.Parse(lenth3.Text));
-        }
-
-        private string GenerateFunction(int length)
-        {
-            Random random = new Random();
-            string result = "";
-
-            for (int i = 0; i < length; i++)
-                result += random.Next(2).ToString();
-
-            return result;
-        }
-
     }
 }
